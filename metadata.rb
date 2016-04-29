e3dbeb1019b4999d1d1661f4c5c4fd979845e769
Joshua Selsky <jselsky@oit.ucla.edu>
2016-04-29 15:24:44 -0700

    version++ because GitHub was out of synch with manage.chef.io.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures mwser-opt'
 long_description 'Installs/Configures mwser-opt'
-version '0.2.22'
+version '0.2.23'
 
 depends 'opt'
 depends 'mysql'
