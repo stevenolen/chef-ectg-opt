37bdc92e85f0754e80bc16a798d7a97e5dfce0fd
Ed Sakabu <sakabu@oit.ucla.edu>
2016-08-03 11:19:33 -0700

    Production to release 2.0.34 - Summer Sprint #1

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,7 +27,7 @@ case node['fqdn']
 when 'onlinepoll.ucla.edu'
   fqdn = 'onlinepoll.ucla.edu'
   app_name = 'prod'
-  app_revision = '2.0.33'
+  app_revision = '2.0.34'
   rails_env = 'production'
   port = 3000
   bridge_enabled = true
