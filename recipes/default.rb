f7a8938075d32312e6d46b5de5e2d343c1ae27e8
Ed Sakabu <EdSakabu@ucla.edu>
2016-08-25 22:02:10 -0700

    Added UCR as a subdomain

--- recipes/default.rb
+++ recipes/default.rb
@@ -51,7 +51,7 @@ when 'test.onlinepoll.ucla.edu' # staging.onlinepoll.ucla.edu
   shib_client = 'test_opt'
 end
 
-subdomains = ['generic']
+subdomains = ['generic', 'ucr']
 
 # install mysql
 db_root_obj = ChefVault::Item.load("passwords", "db_root")
