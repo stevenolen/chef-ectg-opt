4e5bf3038076195bca71a6b8c68413d39cbdd433
Ed Sakabu <EdSakabu@ucla.edu>
2016-09-30 09:06:58 -0700

    Pushing Emergency Security Fix and others...

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,7 +27,7 @@ case node['fqdn']
 when 'onlinepoll.ucla.edu'
   fqdn = 'onlinepoll.ucla.edu'
   app_name = 'prod'
-  app_revision = '2.0.35'
+  app_revision = '2.0.36'
   rails_env = 'production'
   port = 3000
   bridge_enabled = true
