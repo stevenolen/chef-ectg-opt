80a635ae0f8e57aa261cdf726ac9117a4f896f33
Ed Sakabu <EdSakabu@ucla.edu>
2016-09-30 17:14:18 -0700

    Realse to fix Search issued #705 and #714

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures mwser-opt'
 long_description 'Installs/Configures mwser-opt'
-version '0.2.33'
+version '0.2.34'
 
 depends 'opt'
 depends 'mysql'
