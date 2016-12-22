e7241ad41f7790b4b8e8684d6298076618929712
Ed Sakabu <EdSakabu@ucla.edu>
2016-12-21 17:36:46 -0800

    PR #752 - The Big Search Fix

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures mwser-opt'
 long_description 'Installs/Configures mwser-opt'
-version '0.2.38'
+version '0.2.39'
 
 depends 'opt'
 depends 'mysql'
