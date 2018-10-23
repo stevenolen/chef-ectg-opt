65626b29a9dd01ec40596c1bfe503ae21a4552bf
Keith Rozett <krozett@gmail.com>
2018-10-22 18:33:21 -0700

    I must enjoy torturing myself

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,7 +27,7 @@ case node['fqdn']
 when 'onlinepoll.ucla.edu'
   fqdn = 'onlinepoll.ucla.edu'
   app_name = 'prod'
-  app_revision = '2.1.3'
+  app_revision = '2.1.4'
   rails_env = 'production'
   port = 3000
   bridge_enabled = true
