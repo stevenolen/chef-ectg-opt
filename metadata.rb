e67b97204862909c7e003d486f4deba1fb1d020a
Keith Rozett <krozett@gmail.com>
2018-11-27 13:44:54 -0800

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'EdSakabu@ucla.edu'
 license 'Apache 2.0'
 description 'Installs/Configures mwser-opt'
 long_description 'Installs/Configures mwser-opt'
-version '0.2.76'
+version '0.2.77'
 
 
 depends 'opt'
