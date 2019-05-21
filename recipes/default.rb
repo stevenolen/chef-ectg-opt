24ee4a55b59031d2ed151e6f3558d245ddc61728
Keith Rozett <krozett@gmail.com>
2019-05-21 22:19:16 +0900

    Version bump

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,7 +27,7 @@ case node['fqdn']
 when 'onlinepoll.ucla.edu'
   fqdn = 'onlinepoll.ucla.edu'
   app_name = 'prod'
-  app_revision = '2.2.2'
+  app_revision = '2.2.3'
   rails_env = 'production'
   port = 3000
   bridge_enabled = true
