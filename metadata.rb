00a82b4ae4c9af210faa98495ddb69408643980c
Keith Rozett <krozett@gmail.com>
2020-08-18 23:53:42 -0700

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'EdSakabu@ucla.edu'
 license 'Apache 2.0'
 description 'Installs/Configures mwser-opt'
 long_description 'Installs/Configures mwser-opt'
-version '2.4.7'
+version '2.4.8'
 
 
 depends 'opt'
--- recipes/default.rb
+++ recipes/default.rb
@@ -27,7 +27,7 @@ case node['fqdn']
 when 'onlinepoll.ucla.edu'
   fqdn = 'onlinepoll.ucla.edu'
   app_name = 'prod'
-  app_revision = '2.4.7'
+  app_revision = '2.4.8'
   rails_env = 'production'
   port = 3000
   bridge_enabled = true
